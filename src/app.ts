cab26b62108153c60a85df91eb0d4df06ad7c8db
Max B <m.bergmann@students.htl-leonding.ac.at>
2024-04-24 14:46:26 +0200

    updated folder structure

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
 
-import {Database} from './db';
+import {Database} from './api/db';
 import http from "http";
 import express from "express";
-import {sqlRouter} from "./sql-router";
+import {sqlRouter} from "./api/sql-router";
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api', sqlRouter);
+app.use(express.static('website'));
 
 const server = http.createServer(app);
 const port: number = 3000;
