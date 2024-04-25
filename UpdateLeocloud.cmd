6802bb4a12d11e6d8fb1d6ce54cd0df19a75c00f
alinJasic <a.jasic@students.htl-leonding.ac.at>
2024-04-25 14:13:23 +0200

    Added the dockerfile and leocloud-deploy.yaml and UpdateLeocloud.cmd

--- UpdateLeocloud.cmd
+++ UpdateLeocloud.cmd
@@ -1,5 +1,5 @@
-kubectl delete -n student-a-jasic deployment ProjectMatch
-kubectl delete -n student-a-jasic service ProjectMatch-svc
-kubectl delete -n student-a-jasic ingress ProjectMatch-ingress
-kubectl delete -n student-a-jasic pod -l app=ProjectMatch
+kubectl delete -n student-a-jasic deployment projectmatch
+kubectl delete -n student-a-jasic service projectmatch-svc
+kubectl delete -n student-a-jasic ingress projectmatch-ingress
+kubectl delete -n student-a-jasic pod -l app=projectmatch
 kubectl create -f leocloud-deploy-V2.yaml
