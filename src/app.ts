e071656935cdf3a89a7806fbecbdace029e710a4
Max B <m.bergmann@students.htl-leonding.ac.at>
2024-05-08 10:56:32 +0200

    added keycloak stuff

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,29 @@
-
 import {Database} from './api/db';
 import http from "http";
-import express from "express";
+import express from 'express';
+import session from 'express-session';
+import Keycloak from 'keycloak-connect';
 import {userRouter} from "./api/user-router";
 
 const app = express();
+
+const memoryStore = new session.MemoryStore();
+app.use(session({
+    secret: 'some long secret',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+}));
+const keycloak = new Keycloak({ store: memoryStore });
+
+app.use(keycloak.middleware({
+    logout: '/logout'
+}));
+
+app.get('/logout', keycloak.protect(), (req: any, res) => {
+    req.kauth.logout();
+    res.redirect('/');
+});
 
 app.use(express.json());
 
