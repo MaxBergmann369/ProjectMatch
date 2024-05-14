73160261b309aa61ddce70b3a4360d3566a0367a
Adam Höllerl <a.hoellerl@students.htl-leonding.ac.at>
2024-05-14 21:31:43 +0200

    added username to home page and removed console.log from tokenUser class

--- src/website/scripts/home.ts
+++ src/website/scripts/home.ts
@@ -11,8 +11,17 @@ document.addEventListener("DOMContentLoaded", async () => {
         const client = new HttpClient();
         console.log("User is authenticated");
         const user = new TokenUser(keycloak.tokenParsed);
+
+
+        const user1: User | null = await client.getUser(user.userId);
+
+        if(user1 === null) {
+            location.href = "register.html";
+        }
         document.getElementById("username").innerText = `
-            Username: ${user.userId}
+            UserId: ${user.userId}
+            Username: ${user1.username}
+            Born on: ${user1.birthdate}
             Firstname: ${user.firstname}
             Lastname: ${user.lastname}
             Email: ${user.email}
@@ -20,12 +29,6 @@ document.addEventListener("DOMContentLoaded", async () => {
             Class: ${user.class}
             Department: ${user.department}
         `;
-
-        const user1: User | null = await client.getUser(user.userId);
-
-        if(user1 === null) {
-            location.href = "register.html";
-        }
     }
     else {
         console.log("User is not authenticated");
