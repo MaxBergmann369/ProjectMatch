20998934f0db48d3607713a6a80697e9641d2175
Max B <m.bergmann@students.htl-leonding.ac.at>
2024-06-23 11:36:20 +0200

    some fixes

--- website/scripts/notifications.ts
+++ website/scripts/notifications.ts
@@ -6,7 +6,6 @@ import {keycloak} from "./keycloak";
 
 let client: HttpClient = null;
 let user: TokenUser = null;
-let clicked = false;
 
 let notificationsLoaded: Notification[] = [];
 
@@ -41,21 +40,17 @@ function addEventListener() {
     window.addEventListener('click', async (event) => {
         if (event.target !== notifications && event.target !== notificationBox) {
             notificationBox.style.display = 'none';
-            clicked = false;
         }
     });
 
     notifications.addEventListener('click', async () => {
-        console.log(clicked);
-        if(clicked) {
-            notificationBox.style.display = 'none';
-            clicked = false;
-        }
-        else {
-            notificationBox.style.display = 'block';
+        if (notificationBox.style.display === 'none' || notificationBox.style.display === '') {
+            await loadNotifications();
             await renderNotifications(notificationBox);
             await addButtonListeners();
-            clicked = true;
+            notificationBox.style.display = 'block';
+        } else {
+            notificationBox.style.display = 'none';
         }
     });
 }
